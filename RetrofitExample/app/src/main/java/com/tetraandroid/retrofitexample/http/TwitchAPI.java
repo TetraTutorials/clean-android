81d575f3eb325e06ba724554d9a175d491cfc8f5
Rene Gens <renegens@gmail.com>
2016-11-24 20:13:50 +0200

    migrate to new api rules

--- RetrofitExample/app/src/main/java/com/tetraandroid/retrofitexample/http/TwitchAPI.java
+++ RetrofitExample/app/src/main/java/com/tetraandroid/retrofitexample/http/TwitchAPI.java
@@ -8,7 +8,7 @@ import retrofit2.http.GET;
 public interface TwitchAPI {
 
     @GET("games/top")
-    Call <Twitch> getTopGames();
+    Call<Twitch> getTopGames(@Header("Client-Id") String clientId);
 
 
 
