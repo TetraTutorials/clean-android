e4ca8e0d8070d8b407312549bf3c4ba04153b1bf
Rene Gens <renegens@gmail.com>
2017-01-27 00:10:02 +0200

    Lambdas init

---
+++ RetrolambdaExample/app/src/main/java/com/tetraandroid/retrolambdaexample/MainActivity.java
@@ -0,0 +1,31 @@
+package com.tetraandroid.retrolambdaexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private EditText nameEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameEditText = (EditText) findViewById(R.id.submit_edit_text);
+        final Button submitButton = (Button) findViewById(R.id.submit_button);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, String.format("Welcome to Lambdas, %s!", nameEditText.getText().toString()), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+}
