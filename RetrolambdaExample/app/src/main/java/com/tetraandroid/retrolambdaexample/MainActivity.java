38f707912725a68555aff0a043428067001c8637
Rene Gens <renegens@gmail.com>
2017-01-29 16:07:27 +0200

    [+] Retrolambda

--- RetrolambdaExample/app/src/main/java/com/tetraandroid/retrolambdaexample/MainActivity.java
+++ RetrolambdaExample/app/src/main/java/com/tetraandroid/retrolambdaexample/MainActivity.java
@@ -2,13 +2,11 @@ package com.tetraandroid.retrolambdaexample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
 
     private EditText nameEditText;
 
@@ -20,12 +18,16 @@ public class MainActivity extends AppCompatActivity {
         nameEditText = (EditText) findViewById(R.id.submit_edit_text);
         final Button submitButton = (Button) findViewById(R.id.submit_button);
 
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, String.format("Welcome to Lambdas, %s!", nameEditText.getText().toString()), Toast.LENGTH_LONG).show();
-            }
+        submitButton.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, String.format("Welcome to Lambdas, %s!", nameEditText.getText()
+                                                                                                   .toString()), Toast.LENGTH_LONG).show();
+            runnable(() -> System.out.println(String.format("Welcome to Lambdas, %s!", nameEditText.getText()
+                                                                                                   .toString())));
         });
     }
 
+    private void runnable(Runnable runnable) {
+        runnable.run();
+    }
 }
+
